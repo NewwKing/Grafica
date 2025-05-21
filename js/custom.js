2bc504e8cb4c4c13cdebafff06943d1758e69544
Franz Villca <franzvillcaaro12345@gmail.com>
2025-05-21 02:52:06 -0400

    Primer commit

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/proyec.png' },
+              { src: 'images/bloA.jpg' },
+              { src: 'images/bloB.jpg' }
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
